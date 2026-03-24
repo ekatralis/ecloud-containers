890cc05f3540de586205879c0407cb5678a7fe3b
Evangelos Katralis <vkatralis@gmail.com>
2026-03-25 00:05:00 +0100

    Add initial working Dockerfile (1.8GB)

---
+++ setup_pycomplete.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+git clone https://github.com/ekatralis/PyECLOUD
+git clone https://github.com/ekatralis/PyPIC
+git clone https://github.com/ekatralis/PyHEADTAIL
+git clone https://github.com/ekatralis/PyPARIS
+git clone https://github.com/ekatralis/PyPARIS_sim_class
+git clone https://github.com/pycomplete/PyPARIS_CoupledBunch_sim_class
+
+cd PyECLOUD
+./setup_pyecloud
+rm -rf ./testing
+cd ..
+
+cd PyPIC
+git switch replace_bool_idx
+make
+cd ..
+
+cd PyHEADTAIL
+git switch fix_installation_scripts
+make
+rm -rf ./examples
+cd ..
+
+cd PyPARIS
+git switch prevent_infinite_hangs_in_multiproc
+cd ..
