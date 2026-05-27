72316c435803873129166442b9d139c300ec37ff
Evangelos Katralis <vkatralis@gmail.com>
2026-05-27 18:12:57 +0200

    Add initial executable for pyparis simulations

---
+++ HTCondor_templates/PyPARIS_self_resubmitting_jobs/pyparis_executable.sh
@@ -0,0 +1,87 @@
+#!/bin/bash 
+
+set -uo pipefail
+
+ROOT_URL="root://eosproject-e.cern.ch/"
+EOS_PATH="/eos/project/e/ecloud-simulations/ekatrali/test_resubmit/"
+SIM_PATH="$EOS_PATH"
+# CONTAINER_PATH="/cvmfs/"
+# JOB_PWD="$_CONDOR_SCRATCH_DIR"
+CONTAINER_PATH="/cvmfs/"
+JOB_PWD="$_CONDOR_SCRATCH_DIR"
+
+xrdcp_opts=(--retry 3)
+
+transfer_inputs() (
+	set -euo pipefail
+
+	if xrdfs "$ROOT_URL" stat "${SIM_PATH}simulation_status.sta" >/dev/null 2>&1; then
+		xrdcp "${xrdcp_opts[@]}" "${ROOT_URL}${SIM_PATH}simulation_status.sta" ./
+		part_num=$(grep -oP 'present_simulation_part\s*=\s*\K\d+' simulation_status.sta)
+		part=$(printf "%02d" "$part_num")
+		xrdcp "${xrdcp_opts[@]}" "${ROOT_URL}${SIM_PATH}bunch_evolution_${part}.h5" ./
+		xrdcp "${xrdcp_opts[@]}" "${ROOT_URL}${SIM_PATH}slice_evolution_${part}.h5" ./
+		xrdcp "${xrdcp_opts[@]}" "${ROOT_URL}${SIM_PATH}bunch_status_part${part}.h5" ./
+		xrdcp "${xrdcp_opts[@]}" "${ROOT_URL}${SIM_PATH}pyparislog.txt" ./
+		xrdcp "${xrdcp_opts[@]}" "${ROOT_URL}${SIM_PATH}multigrid_config_dip.pkl" ./
+		xrdcp "${xrdcp_opts[@]}" "${ROOT_URL}${SIM_PATH}multigrid_config_dip.txt" ./
+		xrdcp "${xrdcp_opts[@]}" "${ROOT_URL}${SIM_PATH}sim_param.pkl" ./
+		xrdcp "${xrdcp_opts[@]}" "${ROOT_URL}${SIM_PATH}envinfo.txt" ./
+	else
+		echo "No simulation status file found. Initializing simulation..."
+	fi
+
+	# LHC_chm_ver.mat: Either here or centrally
+	xrdcp "${xrdcp_opts[@]}" "${ROOT_URL}${SIM_PATH}Simulation_parameters.py" ./
+	xrdcp "${xrdcp_opts[@]}" -r "${ROOT_URL}${SIM_PATH}pyecloud_config" ./
+
+	apptainer exec --env PYTHONNOUSERSITE=1 --home "$JOB_PWD" "$CONTAINER_PATH" bash -lc 'date; echo $ECLOUD_CONTAINER_VERSION' >> envinfo.txt
+
+
+)
+
+transfer_outputs() (
+	set -euo pipefail
+	part_num=$(grep -oP 'present_simulation_part\s*=\s*\K\d+' simulation_status.sta)
+	part=$(printf "%02d" "$part_num")
+	xrdcp "${xrdcp_opts[@]}" "./bunch_evolution_${part}.h5" "${ROOT_URL}${SIM_PATH}"
+	xrdcp "${xrdcp_opts[@]}" "./slice_evolution_${part}.h5" "${ROOT_URL}${SIM_PATH}"
+	xrdcp "${xrdcp_opts[@]}" "./bunch_status_part${part}.h5" "${ROOT_URL}${SIM_PATH}"
+	xrdcp "${xrdcp_opts[@]}" -f "./pyparislog.txt" "${ROOT_URL}${SIM_PATH}"
+	xrdcp "${xrdcp_opts[@]}" -f "./simulation_status.sta" "${ROOT_URL}${SIM_PATH}"
+	xrdcp "${xrdcp_opts[@]}" -f "./sim_param.pkl" "${ROOT_URL}${SIM_PATH}"
+	xrdcp "${xrdcp_opts[@]}" -f "./stdout.txt" "${ROOT_URL}${SIM_PATH}"
+	xrdcp "${xrdcp_opts[@]}" -f "./stderr.txt" "${ROOT_URL}${SIM_PATH}"
+	xrdcp "${xrdcp_opts[@]}" -f "./multigrid_config_dip.pkl" "${ROOT_URL}${SIM_PATH}"
+	xrdcp "${xrdcp_opts[@]}" -f "./multigrid_config_dip.txt" "${ROOT_URL}${SIM_PATH}"
+	xrdcp "${xrdcp_opts[@]}" -f "./envinfo.txt" "${ROOT_URL}${SIM_PATH}"
+
+	
+	if (( part_num > 1 )); then
+		prev_part=$(printf "%02d" $((part_num - 1)))
+		
+		xrdfs ${ROOT_URL} rm \
+			"${SIM_PATH}bunch_status_part${prev_part}-1.h5"
+	fi
+	
+)
+
+
+if ! transfer_inputs; then
+	echo "[JOB ERROR]: Input file transfer failed"
+	exit 1
+fi
+
+apptainer exec  --env PYTHONNOUSERSITE=1 \
+				--home "$JOB_PWD" \
+				"$CONTAINER_PATH" \
+				python -m PyPARIS.multiprocexec -n 8 sim_class=PyPARIS_sim_class.Simulation.Simulation \
+				>> stdout.txt 2>> stderr.txt
+script_exit=$?
+
+if ! transfer_outputs; then
+	echo "[JOB ERROR]: Output file transfer failed"
+	exit 1
+fi
+
+exit "$script_exit"
